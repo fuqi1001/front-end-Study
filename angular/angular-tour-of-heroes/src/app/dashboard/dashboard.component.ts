2d82d2d2c5bcdf47f961b8fac184e3cc8b01068a
qifu <fuqiqi12345@gmail.com>
2018-01-05 17:28:34 -0500

    CRUD

--- angular/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -15,7 +14,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-  getHeroes() : void {
+  getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
